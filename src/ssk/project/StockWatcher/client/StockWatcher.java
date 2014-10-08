b6e266a0554e479c7d619f333aba538919eea230
sophalkim <sophal.kim.work@gmail.com>
2014-10-07 21:10:29 -0700

    Add Random Stock Prices Dynamically

--- src/ssk/project/StockWatcher/client/StockWatcher.java
+++ src/ssk/project/StockWatcher/client/StockWatcher.java
@@ -3,12 +3,12 @@ package ssk.project.StockWatcher.client;
 import java.util.ArrayList;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyDownEvent;
 import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.i18n.client.NumberFormat;
 import com.google.gwt.user.client.Random;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
@@ -35,23 +35,6 @@ public class StockWatcher implements EntryPoint {
 	private Label lastUpdatedLabel = new Label();
 	private ArrayList<String> stocks = new ArrayList<String>();
 	
-	
-	
-	
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
-			+ "connection and try again.";
-
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
-	 */
-	private final GreetingServiceAsync greetingService = GWT
-			.create(GreetingService.class);
-
 	/**
 	 * This is the entry point method.
 	 */
@@ -165,5 +148,39 @@ public class StockWatcher implements EntryPoint {
 		updateTable(prices);
 	}
 	
+	/**
+	 * Update the Price and Change fields all the rows in the stock table.
+	 * 
+	 * @param prices Stock data for all rows.
+	 */
+	public void updateTable(StockPrice[] prices) {
+		for (int i = 0; i < prices.length; i++) {
+			updateTable(prices[i]);
+		}
+	}
+	
+	/**
+	 * Update a single row in the stock table.
+	 * 
+	 * @param price Stock data for a single row.
+	 */
+	private void updateTable(StockPrice price) {
+		// Make sure the stock is still in the stock table.
+		if (!stocks.contains(price.getSymbol())) {
+			return;
+		}
+		
+		int row = stocks.indexOf(price.getSymbol()) + 1;
+		
+		// Format the data in the Price and Change fields.
+		String priceText = NumberFormat.getFormat("#,##0.00").format(price.getPrice());
+		NumberFormat changeFormat = NumberFormat.getFormat("+#,##0.00;-#,##0.00");
+		String changeText = changeFormat.format(price.getChange());
+		String changePercentText = changeFormat.format(price.getChangePercent());
+		
+		// Populate the Price and Change fields with new data.
+		stocksFlexTable.setText(row, 1, priceText);
+		stocksFlexTable.setText(row, 2, changeText + " (" + changePercentText + "%)");
+	}
 	
 }
