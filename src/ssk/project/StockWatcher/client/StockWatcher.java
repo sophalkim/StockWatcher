e3a24200ef0a4f65c7bfe9e6d8eb36de0aae4d29
sophalkim <sophal.kim.work@gmail.com>
2014-10-08 10:00:04 -0700

    Color changes work

--- src/ssk/project/StockWatcher/client/StockWatcher.java
+++ src/ssk/project/StockWatcher/client/StockWatcher.java
@@ -126,12 +126,14 @@ public class StockWatcher implements EntryPoint {
 		int row = stocksFlexTable.getRowCount();
 		stocks.add(symbol);
 		stocksFlexTable.setText(row, 0, symbol);
+		stocksFlexTable.setWidget(row, 2, new Label());
 		stocksFlexTable.getCellFormatter().addStyleName(row, 1, "watchListNumericColumn");
 		stocksFlexTable.getCellFormatter().addStyleName(row, 2, "watchListNumericColumn");
 		stocksFlexTable.getCellFormatter().addStyleName(row, 3, "watchListRemoveColumn");
 		
 		// Add a button to remove this stock from the table.
 		Button removeStockButton = new Button("x");
+		removeStockButton.addStyleDependentName("remove");
 		removeStockButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
 				int removedIndex = stocks.indexOf(symbol);
@@ -197,7 +199,19 @@ public class StockWatcher implements EntryPoint {
 		
 		// Populate the Price and Change fields with new data.
 		stocksFlexTable.setText(row, 1, priceText);
-		stocksFlexTable.setText(row, 2, changeText + " (" + changePercentText + "%)");
+		Label changeWidget = (Label) stocksFlexTable.getWidget(row, 2);
+		changeWidget.setText(changeText + " (" + changePercentText + "%)");
+		
+		// Change the color of text in the Change field based on its value.
+		String changeStyleName = "noChange";
+		if (price.getChangePercent() <= -0.1f) {
+			changeStyleName = "negativeChange";
+		}
+		else if (price.getChangePercent() > 0.1f) {
+			changeStyleName = "positiveChange";
+		}
+		
+		changeWidget.setStyleName(changeStyleName);
 	}
 	
 }
