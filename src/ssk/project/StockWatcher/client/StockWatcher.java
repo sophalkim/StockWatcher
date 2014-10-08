ef3d4b8cdc98488e8d6e43802a749117d9d5b33c
sophalkim <sophal.kim.work@gmail.com>
2014-10-08 09:49:42 -0700

    All CSS changes working great

--- src/ssk/project/StockWatcher/client/StockWatcher.java
+++ src/ssk/project/StockWatcher/client/StockWatcher.java
@@ -59,6 +59,7 @@ public class StockWatcher implements EntryPoint {
 		// Assemble Add Stock panel.
 		addPanel.add(newSymbolTextBox);
 		addPanel.add(addStockButton);
+		addPanel.addStyleName("addPanel");
 		
 		// Assemble Main panel.
 		mainPanel.add(stocksFlexTable);
@@ -125,9 +126,9 @@ public class StockWatcher implements EntryPoint {
 		int row = stocksFlexTable.getRowCount();
 		stocks.add(symbol);
 		stocksFlexTable.setText(row, 0, symbol);
-		stocksFlexTable.getCellFormatter().addStyleName(0, 1, "watchListNumericColumn");
-		stocksFlexTable.getCellFormatter().addStyleName(0, 2, "watchListNumericColumn");
-		stocksFlexTable.getCellFormatter().addStyleName(0, 3, "watchListRemoveColumn");
+		stocksFlexTable.getCellFormatter().addStyleName(row, 1, "watchListNumericColumn");
+		stocksFlexTable.getCellFormatter().addStyleName(row, 2, "watchListNumericColumn");
+		stocksFlexTable.getCellFormatter().addStyleName(row, 3, "watchListRemoveColumn");
 		
 		// Add a button to remove this stock from the table.
 		Button removeStockButton = new Button("x");
