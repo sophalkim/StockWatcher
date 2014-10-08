a64e169db1f32d314bf6cd2991f060f4da55ee6e
sophalkim <sophal.kim.work@gmail.com>
2014-10-07 19:51:41 -0700

    Implemented Event handlers for click the button and pressing "enter" on the keyboard

--- src/ssk/project/StockWatcher/client/StockWatcher.java
+++ src/ssk/project/StockWatcher/client/StockWatcher.java
@@ -2,10 +2,16 @@ package ssk.project.StockWatcher.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -57,5 +63,39 @@ public class StockWatcher implements EntryPoint {
 		mainPanel.add(stocksFlexTable);
 		mainPanel.add(addPanel);
 		mainPanel.add(lastUpdatedLabel);
+		
+		// Associate the main panel with the HTML host page
+		RootPanel.get("stockList").add(mainPanel);
+		
+		newSymbolTextBox.setFocus(true);
+		
+		// Listen for mouse events on the Add button.
+		addStockButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				addStock();
+			}
+		});
+		
+		// Listen for keyboard events in the input box.
+		newSymbolTextBox.addKeyDownHandler(new KeyDownHandler() {
+			public void onKeyDown(KeyDownEvent event) {
+				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+					addStock();
+				}
+			}
+		});
+		
 	}
+	
+	
+	
+	/**
+	 * Add stock to FlexTable. Executed when the user clicks the addStockButton or
+	 * presses enter in the newSymbolTextBox.
+	 */
+	private void addStock() {
+		
+	}
+	
+	
 }
