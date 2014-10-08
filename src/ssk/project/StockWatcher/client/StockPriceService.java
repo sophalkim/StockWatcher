1c72153c7e9399d51b5f2dee6465bc1f11852774
sophalkim <sophal.kim.work@gmail.com>
2014-10-08 10:57:07 -0700

    Writing server side code

---
+++ src/ssk/project/StockWatcher/client/StockPriceService.java
@@ -0,0 +1,10 @@
+package ssk.project.StockWatcher.client;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+@RemoteServiceRelativePath("stockPrices")
+public interface StockPriceService extends RemoteService {
+
+	StockPrice[] getPrices(String[] symbols);
+}
